0024b6dff3249d43d2e809a9c149dbd20d649eaf
Rémi Vezy <VEZY@users.noreply.github.com>
2024-05-12 16:14:56 +0200

    Update publications.js
    update formating

--- scripts/publications.js
+++ scripts/publications.js
@@ -21,50 +21,8 @@ fetchPublications().then((publications) => {
         const yearB = b.issued["date-parts"][0][0];
         return yearB - yearA;
     }).forEach((publication) => {
-        let authors = publication.author.map((author) => `${author.given} ${author.family}`);
-        // console.log(authors.join(', '));
-        let journal = publication['container-title'];
-        if (journal === undefined) {
-            journal = publication['publisher'];
-        }
-        let doi = publication.DOI;
-        if (doi === undefined) {
-            doi = publication.URL;
-            if (doi === undefined) {
-                doi = publication.ISSN;
-            }
-        }
-        let year = publication.issued["date-parts"][0][0];
-        // <div class="flex flex-col gap-y-3">
-        //     <div class="rounded-lg bg-card text-card-foreground">
-        //         <div class="flex flex-col space-y-1.5">
-        //             <div class="flex items-center justify-between gap-x-2 text-base">
-        //                 <h3 class="font-semibold leading-none">Vezy, R.</h3>
-        //                 <div class="text-sm tabular-nums text-gray-500">2020</div>
-        //             </div>
-        //         </div>
-        //         <div class="text-pretty font-mono text-sm text-muted-foreground mt-2">Dynamic Agroforestry
-        //             Coffee Crop Model (DynACof): A 3D Functional-Structural Plant Model for Coffee Agroforestry
-        //             Systems. <em>Frontiers in Plant Science</em>, 11, 1-15.
-        //         </div>
-        //     </div>
-        // </div>
-
-        // If it is an article:
-        // if (publication.type === 'article-journal') {
-        //     publicationElement.classList.add('article');
-        // }
-        let publicationHTML =`
-        <article class="rounded-lg bg-card text-card-foreground p-3 publication">
-        <p class="font-mono text-sm leading-none">${publication.title}</p>
-        <p class="text-pretty font-mono text-muted-foreground mt-2 text-xs">${authors.join(', ')}</p>
-        <p class="text-pretty font-mono text-muted-foreground mt-2 text-xs">
-            ${year}, ${journal}, 
-            <a href="${doi}" class="hover:underline" target="_blank">${doi}</a>
-        </p>
-        </article>
-        `;
-
+        let publicationHTML = createPublicationHTML(publication);
+        
         if (publication.type === 'article-journal') {
             articleListHTML += publicationHTML;
         } else if (publication.type === 'book' || publication.type === 'chapter') {
@@ -114,4 +72,41 @@ fetchPublications().then((publications) => {
     }
 });
 
+function createPublicationHTML(publication) {
+    let authors = publication.author.map((author) => `${author.given} ${author.family}`);
+    let journal = publication['container-title'];
+    if (journal === undefined) {
+        journal = publication['publisher'];
+    }
+    let doi = publication.DOI;
+    if (doi === undefined) {
+        doi = publication.URL;
+        if (doi === undefined) {
+            doi = publication.ISSN;
+        }
+    }
+    let year = publication.issued["date-parts"][0][0];
 
+    let publicationHTML = `
+    <div class="flex flex-col gap-y-3">
+        <div class="rounded-lg bg-card text-card-foreground p-3">
+            <div class="font-mono text-sm leading-none">
+                ${publication.title}
+                <p class="text-pretty font-medium font-mono text-muted-foreground mt-2 text-xs">${authors.join(', ')}</p>
+            </div>
+            <div class="flex flex-col space-y-1.5">
+                <div class="flex items-center justify-between gap-x-2 text-base">
+                    <div class="text-pretty font-mono text-muted-foreground mt-2 text-xs">
+                        <div class="font-semibold">
+                            ${journal} <a href="${doi}" class="hover:underline font-medium" target="_blank">${doi}</a>
+                        </div>    
+                    </div>    
+                    <div class="text-pretty font-mono text-muted-foreground mt-2 text-xs">${year}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+
+    return publicationHTML;
+}
